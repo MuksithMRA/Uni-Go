9f506f67d354015eeab6119c39360c7b4c1276c5
Abdul Muksith <106472158+MuksithMRA@users.noreply.github.com>
2025-02-13 22:54:21 +0530

    init changes

---
+++ uni go/uni_goApp.swift
@@ -0,0 +1,17 @@
+//
+//  uni_goApp.swift
+//  uni go
+//
+//  Created by HT Works on 2025-02-13.
+//
+
+import SwiftUI
+
+@main
+struct uni_goApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
