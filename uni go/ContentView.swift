53e96bb921e6bb7bd94e31190b9de375d12b97f3
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2025-02-25 13:35:23 +0530

    Folder structure and spalsh screen done.

--- uni go/ContentView.swift
+++ uni go/ContentView.swift
@@ -9,11 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 20) {
+            
+            Text("Welcome To UniGo")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text("Your Campus Navigator")
+                .font(.title2)
+                .foregroundStyle(.gray)
+            
+            Spacer()
+            
+            MyButton(
+                title: "Login",
+                variant: .primary,
+                size: .lg,
+                width: .fixed(200)
+            ){
+                print( "Login pressed" )
+            }
+            MyButton(title: "Login", variant: .secondary, size: .md){
+                print( "Login pressed" )
+            }
+            MyButton(title: "Login", variant: .outline, size: .xs){
+                print( "Login pressed" )
+            }
         }
         .padding()
     }
